2518566d7fb74a9310582d1ded4e7d842e9d5288
Tobias Ecklebe <tobiasecklebe@team-cdlc.de>
2018-06-16 10:51:15 +0200

    Make update of rename script

--- RenameProject.py
+++ RenameProject.py
@@ -1,37 +1,31 @@
-#!/usr/local/bin/python
-# -*- coding: iso-8859-1 -*-
-
 __author__="Tobias Ecklebe"
 
 import os
-import fnmatch
 import fileinput
 import sys
-import getopt
 
-argcount = 0
+def Rename(RootDir, ProjectName, Name2Replace):
+    for subdir, dirs, files in os.walk(RootDir):
+        for filename in files:
+            if not filename.endswith('exe') and not filename.startswith('RenameProject') and not '.git' in subdir and not 'build' in subdir and not 'html' in subdir and not 'pycache' in subdir:
+                print(subdir + '\\' + filename)
+                for line in fileinput.input(subdir + '/' +filename, inplace=1):         
+                    if Name2Replace in line:
+                        line= line.replace(Name2Replace,projectname)
+                        print('hello')
+                    sys.stdout.write(line)   
 
-for arg in sys.argv:
-    argcount = argcount + 1
-
-print(argcount)
-
-if argcount < 2 or argcount > 3:
-    print("Missing name")
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("Missing name")
+        sys.exit(1)
     
-ProjectName = sys.argv[1]
+    ProjectName = sys.argv[1]
+    Name2Replace = sys.argv[2]
+    RootDir = os.getcwd()              
 
-RootDir = os.getcwd()              
-print(RootDir)
-
-searchExp = sys.argv[2]
+    print(Name2Replace)
+    Rename(RootDir, ProjectName, Name2Replace)
             
-for subdir, dirs, files in os.walk(RootDir):
-    for filename in files:
-        if not filename.endswith('exe') and not filename.startswith('RenameProject') and not '.git' in subdir and not 'build' in subdir and not 'html' in subdir and not 'pycache' in subdir:
-            print(subdir + '\\' + filename)
-            for line in fileinput.input(subdir + '/' +filename, inplace=1):         
-                if searchExp in line:
-                    line= line#line.replace(searchexp,projectname)
-                sys.stdout.write(line)   
+if __name__ == '__main__':
+    main()
